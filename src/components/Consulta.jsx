0a18937582306a7d2ea14109004c952087ebdbb9
Nicolas Urman <urmannicolas@gmail.com>
2020-03-06 15:35:13 +0100

    lalalala

--- src/components/Consulta.jsx
+++ src/components/Consulta.jsx
@@ -1,24 +1,75 @@
 import React, { Component } from 'react'
 
 class Consulta extends Component {
+    state = {
+        name: '',
+        phone: '',
+        email: '',
+        message: ''
+    };
+
+    inputOnChange = (e) => {
+        const { name, value } = e.target
+        this.setState({
+        [name]: value
+        })
+    };
+
+    validateForm = (event) => {
+        event.preventDefault()
+        const { name, phone, email, message } = this.state
+        if (name && phone && email && message) {
+        this.isValidEmail(email)
+        } else {
+            this.setState({
+                messageTitle: '',
+                message: 'All fields are required',
+            })
+        }
+    };
+
+    isValidEmail = (email) => {
+        if (/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email)) {
+        this.checkAvailability()
+        } else {
+            this.setState({
+                messageTitle: '',
+                message: "Invalid email",
+            })
+        };
+    } ; 
+
+    submitForm = async () => {
+        const { name, phone, email, message } = this.state;
+        const consulta = { name, phone, email, message};
+        await consulta.newConsulta(consulta);
+        this.setState({
+        name: '',
+        phone: '',
+        email: '',
+        message: ''
+        });
+    };
+
     render() {
+        const {name, phone, email, message} = this.state;
         return (
-            <form className='form'>
+            <form onSubmit={this.submitForm} className='form'>
                 <div className='form-field'>
-                    <label>Nombre</label>
-                    <input />
+                    <label htmlFor="name">Nombre</label>
+                    <input type="text" onChange={this.inputOnChange} required/>
                 </div>   
                 <div className='form-field'>
-                    <label>Teléfono</label>
-                    <input />
+                    <label htmlFor="phone">Teléfono</label>
+                    <input type="text" onChange={this.inputOnChange} required/>
                 </div>   
                 <div className='form-field'>
-                    <label>Correo electrónico</label>
-                    <input />
+                    <label htmlFor="email">Correo electrónico</label>
+                    <input rtype="text" onChange={this.inputOnChange} required/>
                 </div>   
                 <div className='form-field'>
-                    <label>Mensaje</label>
-                    <textarea />
+                    <label htmlFor="message">Mensaje</label>
+                    <textarea type="text" onChange={this.inputOnChange} required/>
                 </div>   
                 <div className='form-field'>
                     <input type='submit' value='Enviar' />
